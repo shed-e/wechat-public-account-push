3fca4d697aa624ca958e87c1d4fe82fadc61e4ed
Shed-e <51053621+shed-e@users.noreply.github.com>
2022-09-13 18:01:08 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -11,8 +11,7 @@ export const config = {
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "15f9bd8f8b384a2332bb7711c7ef733b
-",
+  APP_SECRET: "15f9bd8f8b384a2332bb7711c7ef733b",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
